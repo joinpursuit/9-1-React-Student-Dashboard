9dcb04c3f10dda61124bb15ea224d512c203f5cd
Gillesedgy <edgygilles@pursuit.org>
2022-10-20 15:37:34 -0400

    student list CARD 75% complete... (MISSING total)  / NO CSS

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Header from "./Components/Header";
 import Cohort from "./Components/Cohort";
 import Footer from "./Components/Footer";
 function App() {
+  const [showOption, setShowOption] = useState("");
   return (
-    <>
-      {/* <div className="Records">{Records.map((record) => record.name)}</div> */}
-      <div className="App">
-        <Header />
-        <div className="Main">
-          <Cohort />
-          <StudentList />
-        </div>
-        <Footer></Footer>
+    <div className="App">
+      {/* {console.log(Records[0])} */}
+
+      <Header />
+      <div className="Main">
+        <Cohort />
+        <StudentList Records={Records} />
       </div>
-    </>
+      <Footer></Footer>
+    </div>
   );
 }
 
--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,11 +1,33 @@
-export default function StudentList() {
+export default function StudentList({ Records }) {
   return (
     <div className="StudentList">
-      <h3>All Students</h3>
-      <p>Total Students:{}</p>
-      <div className="Cards">
+      {/* <h3>All Students</h3>
+      <p>Total Students:{}</p> */}
+      <div className="List">
         cards with students
-        {}
+        {Records.map((record) => {
+          return (
+            <div className="Cards" key={record.id}>
+              <img src={record.profilePhoto} alt={record.names} />
+              <div className="UserInfo">
+                <h5>
+                  {record.names.preferredName}{" "}
+                  {record.names.middleName.charAt(0)}. {record.names.surname}
+                </h5>
+                <p>
+                  {record.username} <br /> Birthday:
+                  {record.dob}
+                </p>
+                {/* onClick event to link */}
+                <a style={{ color: "green" }} href="http://">
+                  Show More...
+                </a>
+              </div>
+
+              <p></p>
+            </div>
+          );
+        })}
       </div>
       <br />
       <br />
