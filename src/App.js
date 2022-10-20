2d9e44f0772225555bdf9a298edb51ffd13b9283
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-20 12:43:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import studentData from "./data/data.json";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Student Dashboard</h1>
+      <header>
+        <h1>Student Dashboard</h1>
+      </header>
+      <main>
+        <section className="classes">
+          <h2>Choose a Class by Start Date</h2>
+        </section>
+        <section className="students">
+          <h2>All Students</h2>
+          <p>Total Students: {"placeholder"}</p>
+        </section>
+      </main>
     </div>
   );
 }
