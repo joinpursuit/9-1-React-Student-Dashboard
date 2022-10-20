017c2efceec364630481a005dd01627a9d4dfe5f
TrianePeart <TrianePeart@gmail.com>
2022-10-20 16:31:13 -0400

    safety push

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Header from "./components/Header";
 import Cohort from './components/Cohort'
 import Students from './components/Students'
 import "./App.css"
+import { useState } from "react";
+import data from './data/data.json'
 
 function App() {
+  const [student, setStudent] = useState(data)
+  const [cohorts, setCohorts] = useState('All Students')
+
+  
   return (
     <div className="wrap">
      <Header/>
--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -3,7 +3,7 @@ export default function Cohort(){
     //Should map through to find all the years then should display it in multiples
     return(
         <div className='classDate'>
-            <h2>Choose a Class By Start Date</h2>
+            <h2>Choose a Class by Start Date</h2>
             <ul>
                 <li><button>math for student buttons</button></li>
                 <li><button>math for student buttons</button></li>
