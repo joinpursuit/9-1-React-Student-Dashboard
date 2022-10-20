224d10bcd759794fd8b211263f2fafd64e3c5cba
Salinamalek <salinamalek@pursuit.org>
2022-10-20 13:30:06 -0400

    started project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
+import "./App.css";
 function App() {
   return (
-    <div>
-      <h1>Student Dashboard</h1>
+    <div className="all">
+      <div className="header">
+        <h1>Student Dashboard</h1>
+      </div>
+      <div className="cohort">
+        <h2>Choose a Class by Start Date</h2>
+        <li>All Students</li>
+        <hr></hr>
+        <li>Winter 2026</li>
+        <hr></hr>
+        <li>Fall 2026</li>
+        <hr></hr>
+        <li>Summer 2026</li>
+        <hr></hr>
+        <li>Spring 2026</li>
+        <hr></hr>
+        <li>Winter 2025</li>
+        <hr></hr>
+        <li>Fall 2025</li>
+        <hr></hr>
+        <li>Summer 2025</li>
+        <hr></hr>
+        <li>Spring 2025</li>
+      </div>
     </div>
   );
 }
