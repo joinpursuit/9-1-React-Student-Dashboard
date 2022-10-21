aa92c2b6e3afa692526295b1fb38808e2cec739f
bowersjuan <bowersjuan@gmail.com>
2022-10-21 00:11:34 -0400

    Create static html template and added styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Student Dashboard</h1>
+      <header>Student Dashboard</header>
+      <main className="main">
+        <aside className="cohortsList">
+          <h2>Choose a Class by Start date</h2>
+          <ul>
+            <li>All coursed</li>
+            <li>Winter2026</li>
+            {/* ... */}
+          </ul>
+        </aside>
+        <div className="students">
+          <h2>All Students</h2>
+          <p>Total: 250</p>
+          <div>Student1</div>
+          <div>Student1</div>
+          <div>Student1</div>
+          <div>Student1</div>
+        </div>
+      </main>
     </div>
   );
 }
