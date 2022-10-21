1de9c5087e2f980fc16d3c6579bf168d0b080342
Gillesedgy <edgygilles@pursuit.org>
2022-10-20 22:12:36 -0400

    student list CARD 78% complete... (MISSING total)  / NO CSS

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,23 +1,25 @@
 export default function StudentList({ Records }) {
   return (
     <div className="StudentList">
-      {/* <h3>All Students</h3>
-      <p>Total Students:{}</p> */}
+      <h3>All Students</h3>
+      <p>Total Students:{}</p>
       <div className="List">
-        cards with students
         {Records.map((record) => {
           return (
-            <div className="Cards" key={record.id}>
-              <img src={record.profilePhoto} alt={record.names} />
-              <div className="UserInfo">
-                <h5>
-                  {record.names.preferredName}{" "}
-                  {record.names.middleName.charAt(0)}. {record.names.surname}
-                </h5>
+            <div className="UserInfo">
+              <div className="Cards" key={record.id}>
+                <img src={record.profilePhoto} alt={record.names} />
+                {/* <h5> */}
                 <p>
-                  {record.username} <br /> Birthday:
-                  {record.dob}
+                  <b>
+                    {" "}
+                    {record.names.preferredName}{" "}
+                    {record.names.middleName.charAt(0)}. {record.names.surname}{" "}
+                  </b>
+                  <br /> {/* </h5> */}
+                  {record.username} <br /> Birthday:{record.dob}
                 </p>
+                <br />
                 {/* onClick event to link */}
                 <a style={{ color: "green" }} href="http://">
                   Show More...
