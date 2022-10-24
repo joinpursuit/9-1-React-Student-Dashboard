2c75a739491a4cc486fe3f46f142b5d2b0fe99c0
bowersjuan <bowersjuan@gmail.com>
2022-10-23 22:34:15 -0400

    Add chronological order display to Cohorts

--- src/Components/Cohorts.js
+++ src/Components/Cohorts.js
@@ -1,15 +1,43 @@
 import "./Cohorts.css";
 
 export default function Cohorts({ uniqueCohorts, chooseCohort }) {
+  const seasons = {
+    Winter2026: 1,
+    Fall2026: 2,
+    Summer2026: 3,
+    Spring2026: 4,
+    Winter2025: 5,
+    Fall2025: 6,
+    Summer2025: 7,
+    Spring2025: 8,
+  };
+
+  const orderedCohorts = uniqueCohorts
+    .map((cohort) => {
+      return seasons[cohort];
+    })
+    .sort((a, b) => {
+      return b - a;
+    })
+    .map((a) => {
+      return Object.keys(seasons).find((key) => seasons[key] === a);
+    })
+    .reverse();
+
+  function formatCohortName(cohort) {
+    let idx = cohort.indexOf(20);
+    return cohort.split(20).join(" 20");
+  }
+
   return (
     <div className="cohorts">
       {"<Cohorts />"}
       <h2>Choose a Class by Start date</h2>
 
       <li onClick={() => chooseCohort("All Courses")}>All Courses</li>
-      {uniqueCohorts.map((cohort, idx) => (
+      {orderedCohorts.map((cohort, idx) => (
         <li key={idx} onClick={() => chooseCohort(cohort)}>
-          {cohort}
+          {formatCohortName(cohort)}
         </li>
       ))}
     </div>
