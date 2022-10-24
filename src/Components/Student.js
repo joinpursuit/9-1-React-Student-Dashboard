65d87f87ef60cc06d721896fb0d8b44d8cacd27b
bowersjuan <bowersjuan@gmail.com>
2022-10-23 23:26:11 -0400

    Add CodeWars scores display to Students

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -4,10 +4,14 @@ import StudentInfo from "./StudentInfo";
 export default function Student({ student }) {
   const [showMore, setShowMore] = useState(false);
 
-  function formatDob(date) {
-    let newDate = new Date(date);
-    const newDateArray = newDate.toDateString().split(" ");
-    return `${newDateArray[1]} ${newDateArray[2]}, ${newDateArray[3]}`;
+  const { codewars } = student;
+  const { current } = codewars;
+  const { goal } = codewars;
+
+  function formatCodeWarsPercentage(current, goal) {
+    let res = (current.total / goal.total) * 100;
+    console.log(goal);
+    return res.toFixed(0);
   }
 
   return (
@@ -20,7 +24,18 @@ export default function Student({ student }) {
         <>
           <hr></hr>
           <section className="showMoreSection">
-            <div>Stats</div>
+            <div>
+              <section>
+                <h4>CodeWars</h4>
+                <p>Current Total: {current.total}</p>
+                <p>Last Week: {student.codewars.current.lastWeek}</p>
+                <p>Goal: {goal.total}</p>
+                <p>
+                  Percent of Goal Achieved:{" "}
+                  {formatCodeWarsPercentage(current, goal)}%
+                </p>
+              </section>
+            </div>
             <hr></hr>
             <form className="form"></form>
             <div className="commenets">Comments</div>
