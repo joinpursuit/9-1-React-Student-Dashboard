8fe7bbc54ed186839c151b6b6d162ac51ed96b86
bowersjuan <bowersjuan@gmail.com>
2022-10-23 22:53:06 -0400

    Update Student formatting

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -3,6 +3,12 @@ import { useState } from "react";
 export default function Student({ student }) {
   const [showMore, setShowMore] = useState(false);
 
+  function formatDob(date) {
+    let newDate = new Date(date);
+    const newDateArray = newDate.toDateString().split(" ");
+    return `${newDateArray[1]} ${newDateArray[2]}, ${newDateArray[3]}`;
+  }
+
   return (
     <li className="student">
       <div className="profile">
@@ -13,15 +19,15 @@ export default function Student({ student }) {
           width="50px"
         />
         <div>
-          <p>
-            Name: <span id="firstName">{student.names.preferredName} </span>
+          <h3>
+            <span id="firstName">{student.names.preferredName} </span>
             <span id="middleName">
               {student.names.middleName.slice(0, 1)}.{" "}
             </span>
             <span id="lastName">{student.names.surname}</span>
-          </p>
-          <p>Email: {student.username}</p>
-          <p>Birthday: {student.dob}</p>
+          </h3>
+          <p>{student.username}</p>
+          <p>Birthday: {formatDob(student.dob)}</p>
         </div>
       </div>
       <p className="showMoreButton" onClick={() => setShowMore(!showMore)}>
