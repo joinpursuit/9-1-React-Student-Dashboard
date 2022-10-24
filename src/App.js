648a44c67785c99630c092e440ddccda49987f33
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-10-24 19:57:57 -0400

    still need a button to fire array population

--- src/App.js
+++ src/App.js
@@ -5,28 +5,39 @@ import Cohorts from "./Components/Cohorts";
 import data from "./data/data";
 
 function App() {
+  //********* STATES *********/
   const [cohorts, setCohorts] = useState([]);
+
+  //********* NON-STATE VARS *********/
+
   let cohortArr = [];
 
-  const cohortListAside = () => {
+  //********* HELPERS *********/
+
+  //1. cohortListAside loops through data and sets the value of cohortArr to the be the value of the cohortCode for each element in the original array.
+  //2. then creates a new set (cohortSet to remove the duplicates)
+  //3. set is then converted back into array form (arrOfCohorts)
+  //4. finally sets state of setCohorts to be the value of arrOfCohorts
+  const cohortListAside = (e) => {
+    // e.preventDefault();
     data.forEach((el) => {
       cohortArr = [...cohortArr, el.cohort.cohortCode];
     });
     const cohortSet = new Set(cohortArr);
     const arrOfCohorts = Array.from(cohortSet);
-    setCohorts([...cohorts, arrOfCohorts]);
+    setCohorts([arrOfCohorts]);
 
     console.log("###state###", cohorts);
   };
 
-  // cohortListAside();
+  //********** RETURN **********/
 
   return (
-    <div className="page">
+    <div className="page" onLoad={cohortListAside}>
       <Header />
       <Cohorts cohorts={cohorts} cohortListAside={cohortListAside} />
       <Students students={data} />
-      {/* <button onClick={cohortList}>click</button> */}
+      <button onClick={cohortListAside}>click</button>
     </div>
   );
 }
--- src/Components/Cohorts.js
+++ src/Components/Cohorts.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 const Cohorts = ({ cohorts, cohortListAside }) => {
-  //   console.log(cohorts);
-  cohortListAside()();
+  console.log(cohorts);
+  //   cohortListAside();
+
+  const cohortLi = () => {
+    cohorts.map((cohort) => {
+      return <li>{cohort}</li>;
+    });
+  };
+
   return (
-    <div className="aside">
-      <h1>Cohorts</h1>
+    <div className="aside" onLoad={cohortListAside}>
+      <ul>{cohorts}</ul>
     </div>
   );
 };
