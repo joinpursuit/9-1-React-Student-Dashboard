8e2f58149787888fe6867c63ae28fece7fde4cb0
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-10-24 12:54:52 -0400

    pushing to verify error

--- src/Students.js
+++ src/Students.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Form from './Form';
 import './Students.css';
 
 function Students({ Data }) {
   const studentDetails = Data.map((el) => {
+    const dob = new Date(el.dob);
+    const birthday = new Intl.DateTimeFormat('en-US', {
+      dateStyle: 'long',
+    }).format(dob);
+
+    // console.log(birthday);
     return (
       <article key={el.id} className="article">
         <img
@@ -10,16 +17,19 @@ function Students({ Data }) {
           src={el.profilePhoto}
           alt={el.names.preferredName}
         />
-        <div className="individual-card">
-          <h3>
-            {el.names.preferredName} {el.names.middleName.charAt(0)}.{' '}
-            {el.names.surname}
-          </h3>
-          <p>{el.username}</p>
-          <p>
-            <span>Birthday:</span> {el.dob}
-          </p>
-          <button className="student-show-more">Show more ...</button>
+        <div>
+          <aside className="individual-card">
+            <h3>
+              {el.names.preferredName} {el.names.middleName.charAt(0)}.{' '}
+              {el.names.surname}
+            </h3>
+            <p>{el.username}</p>
+            <p>
+              <span>Birthday:</span> {birthday}
+            </p>
+            <button className="student-show-more">Show more ...</button>
+          </aside>
+          <Form Data={Data} />
         </div>
       </article>
     );
