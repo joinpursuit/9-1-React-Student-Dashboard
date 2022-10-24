3d4996221f15c71bbc7196002c31da4ff086b8f0
Lennie Nurse <lnursej@gmail.com>
2022-10-24 19:23:23 -0400

    "update"

--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -6,24 +6,23 @@ function StudentsList({ data }) {
     <div className="students">
       <h2>All Students</h2>
       <h3>Total Students: {data.length}</h3>
-      <ol>
-        {data.map((dataList) => {
-          return (
-            <li key={dataList.id}>
-              <img src={dataList.profilePhoto} alt={dataList.id} />
-              <strong>
-                {dataList.names.preferredName} {dataList.names.middleName}.{" "}
-                {dataList.names.surname}
-              </strong>
-              <br></br>
-              {dataList.username}
-              <br></br>
-              Birthday: {dataList.dob}
-              <button>Show me</button>
-            </li>
-          )
-        })}
-      </ol>
+
+      {data.map((dataList) => {
+        return (
+          <div key={dataList.id}>
+            <img src={dataList.profilePhoto} alt={dataList.id} />
+            <strong>
+              {dataList.names.preferredName} {dataList.names.middleName}.{" "}
+              {dataList.names.surname}
+            </strong>
+            <br></br>
+            {dataList.username}
+            <br></br>
+            Birthday: {dataList.dob}
+            <button>Show me</button>
+          </div>
+        )
+      })}
     </div>
   )
 }
