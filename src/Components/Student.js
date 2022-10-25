ad78bdb3d8a24d95d18428e604c07a0d9afb866e
sreeves07 <shamarreeves@pursuit.org>
2022-10-25 14:11:21 -0400

    birthday updated and show more functionality in progress

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -2,6 +2,13 @@ import React, {useState} from 'react';
 import Data from '../data/data.json'
 
 const Student = () => {
+    const [clicked, setClicked] = useState(false)
+    const [studentDetailsId, setStudentDetailsId] = useState("")
+
+    function handleStudentDetails(e) {
+        setStudentDetailsId(e.target.value)
+        setClicked(!clicked)
+    }
     function percentScore(num1,num2) {
         return Math.round((num1/num2) *100)
     }
@@ -13,15 +20,26 @@ const Student = () => {
             return "✅"
         }
     }
+
+    function getBirthday() {
+        const date = new Date()
+
+        return date.toLocaleDateString('en-US', {
+            year:  'numeric',
+            month: 'long',
+            day:   'numeric',
+        })
+    }
+
     const students = Data.map((data) => {
         return (
             <div className="cards" style={{boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)", margin: "8px", padding: "20px", borderRadius: "8px"}}>
                     <div className='students'>
                         <h4>{data.names.preferredName} {`${data.names.middleName[0].toUpperCase()}.`} {data.names.surname}</h4>
                         <p>{data.username}</p>
-                        <p>Birthday: {data.dob}</p>
+                        <p>Birthday: {getBirthday(data.dob)}</p>
                         <img className="photo" src={data.profilePhoto} alt="" style={{height: "140px", width:"140px"}} />
-                        <p>Show More...</p>
+                        <a href="#" className='studentDetails' onClick={(event) => {handleStudentDetails(event)}}>Show More...</a>
                         <section>
                             <h4>Codewars</h4>
                             <p>Current Total: {data.codewars.current.total}</p>
