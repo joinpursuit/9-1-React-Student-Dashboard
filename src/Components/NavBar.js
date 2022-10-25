1682130a52f2bb85548a6ee0ee1e2b5005fbc9c3
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-25 13:09:15 -0400

    successful addition of nav bar component with working links

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NavBar(props) {
+    return (
+        <nav>
+            <a
+            href='https://github.com/DestinyJoyner'
+            target="_blank">Git Hub</a>
+            <a
+            href='https://www.linkedin.com/in/destiny-joyner-934846243/'
+            target="_blank">LinkedIn</a>
+            <a
+            href="mailto:destinyjoyner@pursuit.org">Contact Me</a>
+        </nav>
+    );
+}
+
+export default NavBar;
