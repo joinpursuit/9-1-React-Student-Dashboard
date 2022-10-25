6aa7fc5b5ab17ed29048c283610b6532e471b3bd
sreeves07 <shamarreeves@pursuit.org>
2022-10-25 17:06:13 -0400

    show more updated

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -2,13 +2,13 @@ import React, {useState} from 'react';
 import Data from '../data/data.json'
 
 const Student = () => {
-    const [clicked, setClicked] = useState(false)
-    const [studentDetailsId, setStudentDetailsId] = useState("")
 
-    function handleStudentDetails(e) {
-        setStudentDetailsId(e.target.value)
-        setClicked(!clicked)
-    }
+    const [showDetails, setShowDetails] = useState(false)
+
+    function toggleStudentDetails() {
+        setShowDetails(!showDetails);
+      }
+
     function percentScore(num1,num2) {
         return Math.round((num1/num2) *100)
     }
@@ -39,23 +39,29 @@ const Student = () => {
                         <p>{data.username}</p>
                         <p>Birthday: {getBirthday(data.dob)}</p>
                         <img className="photo" src={data.profilePhoto} alt="" style={{height: "140px", width:"140px"}} />
-                        <a href="#" className='studentDetails' onClick={(event) => {handleStudentDetails(event)}}>Show More...</a>
-                        <section>
-                            <h4>Codewars</h4>
-                            <p>Current Total: {data.codewars.current.total}</p>
-                            <p>Last Week: {data.codewars.current.lastWeek}</p>
-                            <p>Goal: {data.codewars.goal.total}</p>
-                            <p>Percent of Goal Achieved: {percentScore(data.codewars.current.total, data.codewars.goal.total)}%</p>
-                            <h4>Scores</h4>
-                            <p>Assignments: {data.cohort.scores.assignments * 100}%</p>
-                            <p>Projects: {data.cohort.scores.projects * 100}%</p>
-                            <p>Assessments: {data.cohort.scores.assessments * 100}%</p>
-                            <h4>Certifications</h4>
-                            <p>Resume: {checkCertificate(data.certifications.resume)}</p>
-                            <p>LinkedIn: {checkCertificate(data.certifications.linkedin)}</p>
-                            <p>Mock Interview: {checkCertificate(data.certifications.mockInterview)}</p>
-                            <p>Github: {checkCertificate(data.certifications.github)}</p>
-                        </section>
+                        <div>
+                            <a href="#"  onClick={()=>{toggleStudentDetails(data)}}>{!showDetails ? "Show More..." : "Show Less..."}</a>
+                                {showDetails ? (
+                                <div className="student-details">
+                                <section>
+                                    <h4>Codewars</h4>
+                                    <p>Current Total: {data.codewars.current.total}</p>
+                                    <p>Last Week: {data.codewars.current.lastWeek}</p>
+                                    <p>Goal: {data.codewars.goal.total}</p>
+                                    <p>Percent of Goal Achieved: {percentScore(data.codewars.current.total, data.codewars.goal.total)}%</p>
+                                    <h4>Scores</h4>
+                                    <p>Assignments: {data.cohort.scores.assignments * 100}%</p>
+                                    <p>Projects: {data.cohort.scores.projects * 100}%</p>
+                                    <p>Assessments: {data.cohort.scores.assessments * 100}%</p>
+                                    <h4>Certifications</h4>
+                                    <p>Resume: {checkCertificate(data.certifications.resume)}</p>
+                                    <p>LinkedIn: {checkCertificate(data.certifications.linkedin)}</p>
+                                    <p>Mock Interview: {checkCertificate(data.certifications.mockInterview)}</p>
+                                    <p>Github: {checkCertificate(data.certifications.github)}</p>
+                                </section>
+                            </div>
+                        ) : null}
+                        </div>
                     </div>
                 </div>
         )
--- src/Components/StudentDetails.js
+++ src/Components/StudentDetails.js
@@ -4,30 +4,57 @@ import Data from '../data/data.json'
 const StudentDetails = () => {
     const [showDetails, setShowDetails] = useState(false)
 
-    function toggleDogDetails() {
+    function toggleStudentDetails() {
         setShowDetails(!showDetails);
       }
+
+      function percentScore(num1,num2) {
+        return Math.round((num1/num2) *100)
+    }
+
+    function checkCertificate(x) {
+        if (x === true) {
+            return "❌"
+        } else {
+            return "✅"
+        }
+    }
+
+    function getBirthday() {
+        const date = new Date()
+
+        return date.toLocaleDateString('en-US', {
+            year:  'numeric',
+            month: 'long',
+            day:   'numeric',
+        })
+    }
 
     Data.map((data) => {
         return (
             <div>
-            <button onClick={toggleDogDetails}>
-                {!showDetails ? "Show More..." : "Show Less..."}
-            </button>
-            {showDetails ? (
-            <div className="student-details">
-            <span>
-                <h3>Codewars:</h3>
-                <p>Current Total: </p>
-
-
-            </span>
+                <a href="#"  onClick={toggleStudentDetails}>{!showDetails ? "Show More..." : "Show Less..."}</a>
+                {showDetails ? (
+                <div className="student-details">
+                        <section>
+                            <h4>Codewars</h4>
+                            <p>Current Total: {data.codewars.current.total}</p>
+                            <p>Last Week: {data.codewars.current.lastWeek}</p>
+                            <p>Goal: {data.codewars.goal.total}</p>
+                            <p>Percent of Goal Achieved: {percentScore(data.codewars.current.total, data.codewars.goal.total)}%</p>
+                            <h4>Scores</h4>
+                            <p>Assignments: {data.cohort.scores.assignments * 100}%</p>
+                            <p>Projects: {data.cohort.scores.projects * 100}%</p>
+                            <p>Assessments: {data.cohort.scores.assessments * 100}%</p>
+                            <h4>Certifications</h4>
+                            <p>Resume: {checkCertificate(data.certifications.resume)}</p>
+                            <p>LinkedIn: {checkCertificate(data.certifications.linkedin)}</p>
+                            <p>Mock Interview: {checkCertificate(data.certifications.mockInterview)}</p>
+                            <p>Github: {checkCertificate(data.certifications.github)}</p>
+                        </section>
+                    </div>
+                ) : null}
             </div>
-        ) : null}
-            
-        </div>
-    
-
         )
     })
        
