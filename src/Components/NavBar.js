0d7ec6719ddbb398a508e17c381ad9ab68d45dd0
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-27 21:08:29 -0400

    added icons to navbar and css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,12 +5,24 @@ function NavBar(props) {
         <nav>
             <a
             href='https://github.com/DestinyJoyner'
-            target="_blank">Git Hub</a>
+            target="_blank">Git Hub
+            <br></br>
+            <img src = {`${process.env.PUBLIC_URL}/assets/gitHub-icon.png`}
+            alt= "gitHub-icon" />
+            </a>
             <a
             href='https://www.linkedin.com/in/destiny-joyner-934846243/'
-            target="_blank">LinkedIn</a>
+            target="_blank">LinkedIn
+            <br></br>
+            <img src = {`${process.env.PUBLIC_URL}/assets/LinkedIn-icon.png`}
+            alt = "LinkedIn-icon"  />
+            </a>
             <a
-            href="mailto:destinyjoyner@pursuit.org">Contact Me</a>
+            href="mailto:destinyjoyner@pursuit.org">Contact Me
+            <br></br>
+            <img src ={`${process.env.PUBLIC_URL}/assets/Mail-icon.png`}
+            alt = "Mail-icon" />
+            </a>
         </nav>
     );
 }
