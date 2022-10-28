fc70771e1f2d3a44d8f4a86cb636c8b178b558fb
bowersjuan <bowersjuan@gmail.com>
2022-10-28 10:42:14 -0400

    Update comments for Cohorts

--- src/Components/Cohorts.js
+++ src/Components/Cohorts.js
@@ -16,9 +16,16 @@ export default function Cohorts({
   const orderedCohorts = uniqueCohorts
     .map((season) => season.split(/([0-9]+)/))
     // Splits cohortCodes into two nested arrays. One array with the season and another with the year, allowing for easier access to each element
+
+    // ["winter2025", "summer2026", ...rest]
+    // -> [["winter", "2025", " "], ["summer", "2026", " "], ...rest]
+
     .sort(([aSeason, aYear], [bSeason, bYear]) => {
       return bYear - aYear || seasons[aSeason] - seasons[bSeason];
     }) // Sorts the cohort codes depending first on the year and then on the season, using the Helper season object as a reference to determine the order of the seasons
+
+    // "or" operator allows us to compare years first and the seasons since it short-ciruits if the difference between the years is not zero, otherwise the seasons are compares by using the helper Object as reference
+
     .map(([season, year]) => {
       return `${season}${year}`;
     }); // returns original structure for cohortCodes but in chronoloical order
